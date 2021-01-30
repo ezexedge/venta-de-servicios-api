2191cee9e9120ce70dbd0895e2596cffa81fb08d
juan gallardo <juan@texdinamo.com>
2021-01-30 16:39:58 -0300

    agregando BASE DE DATOS

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 import express from 'express'
 import {readdirSync} from 'fs'
-
+import mongoose from 'mongoose'
+require('dotenv').config()
+const morgan  = require('morgan')
 
 const app = express()
 
+mongoose.connect(process.env.DATABASE,
+    {
+        useNewUrlParser: true
+        }
+        )
+.then(() => console.log('DB connect'))
+.catch((error)=> console.log('error',error))
 
 
-
+app.use(morgan('dev'))
 readdirSync("./routes").map((r) => 
 app.use("/api", require(`./routes/${r}`))
 )
 
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 
 app.listen(PORT,()=>{
